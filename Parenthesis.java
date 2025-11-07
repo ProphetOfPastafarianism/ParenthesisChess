dc64e506cc682ebc4255fc77329c5a5962c1b75e
ProphetOfPastafarianism <magnushuston123@gmail.com>
2025-11-07 19:29:01 +0000

    day1

---
+++ Parenthesis.java
@@ -0,0 +1,30 @@
+public class Parenthesis{
+//  Class author:  Magnus Huston
+//  Date created:  7/11/2025
+//  General description: Given a String s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid. 
+//An input string is valid if:
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+
+public static void isValid(String test){
+    //ts might not work/probably
+int cresentL= test.indexOf("(");
+    if (test.contains("(")&&test.contains(")"))
+      //use length and index
+      
+    System.out.println("turtle");
+    else
+          System.out.println("fart");
+    
+    
+}
+
+public static void main(String[] args) {
+isValid("(){}[]");
+}
+
+
+
+
+}
