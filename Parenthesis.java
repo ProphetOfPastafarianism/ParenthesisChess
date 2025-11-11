610182b33d8d6ba35512a78033649bd214b5d2f5
ProphetOfPastafarianism <magnushuston123@gmail.com>
2025-11-11 19:29:59 +0000

    dfsdgd

--- Parenthesis.java
+++ Parenthesis.java
@@ -6,22 +6,23 @@ public class Parenthesis{
 //Open brackets must be closed by the same type of brackets.
 //Open brackets must be closed in the correct order.
 //Every close bracket has a corresponding open bracket of the same type.
-
+//SPACE NOT ALLOWED MEMBER
+//4 things it cant be for testing
 public static void isValid(String test){
-    //ts might not work/probably
-int cresentL= test.indexOf("(");
-    if (test.contains("(")&&test.contains(")"))
-      //use length and index
-      
-    System.out.println("turtle");
+    //ts will work/probably
+    if (test.contains("()")||test.contains("[]")||test.contains("{}")){
+      if (test.indexOf("("+1)!=test.indexOf("}")&&test.indexOf("("+1)!=test.indexOf("]"))
+        if (test.indexOf("["+1)!=test.indexOf(")")&&test.indexOf("["+1)!=test.indexOf("}"))
+          if (test.indexOf("{"+1)!=test.indexOf(")")&&test.indexOf("{"+1)!=test.indexOf("]"))
+            System.out.println("i did it!");
     else
           System.out.println("fart");
-    
+    }
     
 }
 
 public static void main(String[] args) {
-isValid("(){}[]");
+isValid("({)[]");
 }
 
 
