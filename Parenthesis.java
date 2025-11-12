41926ca1f88778fb123f0e23a240fdbe4e6b5f01
ProphetOfPastafarianism <magnushuston123@gmail.com>
2025-11-12 19:17:30 +0000

    doneish

--- Parenthesis.java
+++ Parenthesis.java
@@ -2,27 +2,31 @@ public class Parenthesis{
 //  Class author:  Magnus Huston
 //  Date created:  7/11/2025
 //  General description: Given a String s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid. 
-//An input string is valid if:
+//An input string is valid if: it has just the characters mentioned above and they are all closed in order
 //Open brackets must be closed by the same type of brackets.
 //Open brackets must be closed in the correct order.
 //Every close bracket has a corresponding open bracket of the same type.
+//below is giberish by me
 //SPACE NOT ALLOWED MEMBER
 //4 things it cant be for testing
+//giberish end
+//  Pre-condition: string must contain only "(",")","[","]","{" and "}"
+// Dont use other words or numbers and only one of each bracket
+//  Post-condition: this will tell you if your string is valid or not
 public static void isValid(String test){
-    //ts will work/probably
-    if (test.contains("()")||test.contains("[]")||test.contains("{}")){
-      if (test.indexOf("("+1)!=test.indexOf("}")&&test.indexOf("("+1)!=test.indexOf("]"))
-        if (test.indexOf("["+1)!=test.indexOf(")")&&test.indexOf("["+1)!=test.indexOf("}"))
-          if (test.indexOf("{"+1)!=test.indexOf(")")&&test.indexOf("{"+1)!=test.indexOf("]"))
-            System.out.println("i did it!");
+  //this tests if it has all the brackets required and then if any of them are in the wrong spot
+      if(test.contains("(")&&test.contains(")")&&test.contains("{")&&test.contains("}")&&test.contains("[")&&test.contains("]")){
+      if (test.indexOf("(")+1!=test.indexOf("}")&&test.indexOf("(")+1!=test.indexOf("]"))
+      if (test.indexOf("[")+1!=test.indexOf(")")&&test.indexOf("[")+1!=test.indexOf("}"))
+      if (test.indexOf("{")+1!=test.indexOf(")")&&test.indexOf("{")+1!=test.indexOf("]"))
+             System.out.println("true");
     else
-          System.out.println("fart");
-    }
-    
+      System.out.println("fart");
+}
 }
 
 public static void main(String[] args) {
-isValid("({)[]");
+isValid("{([])}");
 }
 
 
