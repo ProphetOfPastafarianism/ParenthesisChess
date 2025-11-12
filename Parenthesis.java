70599f26f1989619f538d169dde5d09597285535
ProphetOfPastafarianism <magnushuston123@gmail.com>
2025-11-12 19:28:56 +0000

    it wont fart :(

--- Parenthesis.java
+++ Parenthesis.java
@@ -11,17 +11,18 @@ public class Parenthesis{
 //4 things it cant be for testing
 //giberish end
 //  Pre-condition: string must contain only "(",")","[","]","{" and "}"
-// Dont use other words or numbers and only one of each bracket
+// Dont use other words or numbers 
 //  Post-condition: this will tell you if your string is valid or not
 public static void isValid(String test){
-  //this tests if it has all the brackets required and then if any of them are in the wrong spot
+  //this tests if it has all the brackets required and then if any of them are in the wrong spot and that it doesnt end funky
       if(test.contains("(")&&test.contains(")")&&test.contains("{")&&test.contains("}")&&test.contains("[")&&test.contains("]")){
       if (test.indexOf("(")+1!=test.indexOf("}")&&test.indexOf("(")+1!=test.indexOf("]"))
       if (test.indexOf("[")+1!=test.indexOf(")")&&test.indexOf("[")+1!=test.indexOf("}"))
       if (test.indexOf("{")+1!=test.indexOf(")")&&test.indexOf("{")+1!=test.indexOf("]"))
+      if (test.endsWith("{")==false&&test.endsWith("(")==false&&test.endsWith("[")==false)
              System.out.println("true");
-    else
-      System.out.println("fart");
+             else
+              System.out.println("fart");
 }
 }
 
